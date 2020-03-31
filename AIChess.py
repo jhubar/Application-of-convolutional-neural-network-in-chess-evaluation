18aa6d6797392828ad9a26729bbe4f97672f18bb
Quentin Lowette <quentin.lowette@gmail.com>
2020-03-31 12:04:42 +0200

    Code from the medium article

---
+++ AIChess.py
@@ -0,0 +1,258 @@
+import chess
+import chess.polyglot
+import chess.svg
+
+pawntable = [
+    0,  0,  0,  0,  0,  0,  0,  0,
+    5, 10, 10, -20, -20, 10, 10,  5,
+    5, -5, -10,  0,  0, -10, -5,  5,
+    0,  0,  0, 20, 20,  0,  0,  0,
+    5,  5, 10, 25, 25, 10,  5,  5,
+    10, 10, 20, 30, 30, 20, 10, 10,
+    50, 50, 50, 50, 50, 50, 50, 50,
+    0,  0,  0,  0,  0,  0,  0,  0]
+
+knightstable = [
+    -50, -40, -30, -30, -30, -30, -40, -50,
+    -40, -20,  0,  5,  5,  0, -20, -40,
+    -30,  5, 10, 15, 15, 10,  5, -30,
+    -30,  0, 15, 20, 20, 15,  0, -30,
+    -30,  5, 15, 20, 20, 15,  5, -30,
+    -30,  0, 10, 15, 15, 10,  0, -30,
+    -40, -20,  0,  0,  0,  0, -20, -40,
+    -50, -40, -30, -30, -30, -30, -40, -50]
+
+bishopstable = [
+    -20, -10, -10, -10, -10, -10, -10, -20,
+    -10,  5,  0,  0,  0,  0,  5, -10,
+    -10, 10, 10, 10, 10, 10, 10, -10,
+    -10,  0, 10, 10, 10, 10,  0, -10,
+    -10,  5,  5, 10, 10,  5,  5, -10,
+    -10,  0,  5, 10, 10,  5,  0, -10,
+    -10,  0,  0,  0,  0,  0,  0, -10,
+    -20, -10, -10, -10, -10, -10, -10, -20]
+
+rookstable = [
+    0,  0,  0,  5,  5,  0,  0,  0,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    5, 10, 10, 10, 10, 10, 10,  5,
+    0,  0,  0,  0,  0,  0,  0,  0]
+
+queenstable = [
+    -20, -10, -10, -5, -5, -10, -10, -20,
+    -10,  0,  0,  0,  0,  0,  0, -10,
+    -10,  5,  5,  5,  5,  5,  0, -10,
+    0,  0,  5,  5,  5,  5,  0, -5,
+    -5,  0,  5,  5,  5,  5,  0, -5,
+    -10,  0,  5,  5,  5,  5,  0, -10,
+    -10,  0,  0,  0,  0,  0,  0, -10,
+    -20, -10, -10, -5, -5, -10, -10, -20]
+
+kingstable = [
+    20, 30, 10,  0,  0, 10, 30, 20,
+    20, 20,  0,  0,  0,  0, 20, 20,
+    -10, -20, -20, -20, -20, -20, -20, -10,
+    -20, -30, -30, -40, -40, -30, -30, -20,
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -30, -40, -40, -50, -50, -40, -40, -30]
+
+def evaluate(board):
+    """
+    Evaluate a given position according to the remaining pieces and their position
+
+    Arguments:
+    ----------
+    board : A chess.Board object representing the position to evaluate
+
+    Return:
+    -------
+    A score as an integer from -9999 to 9999
+    """
+    if board.is_checkmate():
+        if board.turn:
+            return -9999
+        else:
+            return 9999
+
+    if board.is_stalemate():
+        return 0
+
+    if board.is_insufficient_material():
+        return 0
+
+    wp = len(board.pieces(chess.PAWN, chess.WHITE))
+    bp = len(board.pieces(chess.PAWN, chess.BLACK))
+    wn = len(board.pieces(chess.KNIGHT, chess.WHITE))
+    bn = len(board.pieces(chess.KNIGHT, chess.BLACK))
+    wb = len(board.pieces(chess.BISHOP, chess.WHITE))
+    bb = len(board.pieces(chess.BISHOP, chess.BLACK))
+    wr = len(board.pieces(chess.ROOK, chess.WHITE))
+    br = len(board.pieces(chess.ROOK, chess.BLACK))
+    wq = len(board.pieces(chess.QUEEN, chess.WHITE))
+    bq = len(board.pieces(chess.QUEEN, chess.BLACK))
+
+    material = 100*(wp-bp)+320*(wn-bn)+330*(wb-bb)+500*(wr-br)+900*(wq-bq)
+
+    pawnsq = sum([pawntable[i] for i in board.pieces(chess.PAWN, chess.WHITE)])
+    pawnsq = pawnsq + sum([-pawntable[chess.square_mirror(i)]
+                           for i in board.pieces(chess.PAWN, chess.BLACK)])
+    knightsq = sum([knightstable[i]
+                    for i in board.pieces(chess.KNIGHT, chess.WHITE)])
+    knightsq = knightsq + sum([-knightstable[chess.square_mirror(i)]
+                               for i in board.pieces(chess.KNIGHT, chess.BLACK)])
+    bishopsq = sum([bishopstable[i]
+                    for i in board.pieces(chess.BISHOP, chess.WHITE)])
+    bishopsq = bishopsq + sum([-bishopstable[chess.square_mirror(i)]
+                               for i in board.pieces(chess.BISHOP, chess.BLACK)])
+    rooksq = sum([rookstable[i]
+                  for i in board.pieces(chess.ROOK, chess.WHITE)])
+    rooksq = rooksq + sum([-rookstable[chess.square_mirror(i)]
+                           for i in board.pieces(chess.ROOK, chess.BLACK)])
+    queensq = sum([queenstable[i]
+                   for i in board.pieces(chess.QUEEN, chess.WHITE)])
+    queensq = queensq + sum([-queenstable[chess.square_mirror(i)]
+                             for i in board.pieces(chess.QUEEN, chess.BLACK)])
+    kingsq = sum([kingstable[i]
+                  for i in board.pieces(chess.KING, chess.WHITE)])
+    kingsq = kingsq + sum([-kingstable[chess.square_mirror(i)]
+                           for i in board.pieces(chess.KING, chess.BLACK)])
+
+    eval = material + pawnsq + knightsq + bishopsq + rooksq + queensq + kingsq
+    if board.turn:
+        return eval
+    else:
+        return -eval
+
+def alphabetaMinimax(board, alpha, beta, depth):
+    """
+    Compute the minimax algorithm given a position, values of alpha and beta and a depth
+
+    Arguments:
+    ----------
+    - board : A Board object that represents the position
+    - alpha : The value of the alpha parameter
+    - beta : The value of the beta parameter
+    - depth : The depth in which to search the best move
+
+    Return:
+    -------
+    The score associated to the given board (when a move has been done)
+    """
+    bestScore = -9999
+    if depth == 0:
+        return quiescentSearch(board, alpha, beta)
+
+    for move in board.legal_moves:
+        board.push(move)
+        score = -alphabetaMinimax(board, -beta, -alpha, depth - 1)
+        board.pop()
+
+        if score >= beta:
+            return score
+        if score > bestScore:
+            bestScore = score
+        if score > alpha:
+            alpha = score
+    return bestScore
+
+def quiescentSearch(board, alpha, beta):
+    """
+    Performs a quiescent search to avoid the Horizon Effect.
+    Avoid the AI to make bad move because it didn't see a capture in what follows.
+
+    Arguments:
+    ----------
+    - board : A Board object that represents the position
+    - alpha : The value of the alpha parameter
+    - beta : The value of the beta parameter
+
+    Return:
+    -------
+    The evaluation of the board for the best move to do
+    """
+    score = evaluate(board)
+    if score >= beta:
+        return beta
+    if score > alpha:
+        alpha = score
+
+    for move in board.legal_moves:
+        if board.is_capture(move):
+            board.push(move)
+            score = -quiescentSearch(board, -beta, -alpha)
+            board.pop()
+
+            if score >= beta:
+                return beta
+            if score > alpha:
+                alpha = score
+    return alpha
+
+def searchNextMove(board, depth):
+    """
+    Searches the given board for thebest move to play
+
+    Arguments:
+    ----------
+    - board : A Board object to search in
+    - depth : The maximum depth at which the AI need to search
+
+    Return:
+    -------
+    A Move object that represents the best move to play
+    """
+    bestMove = chess.Move.null()
+    bestValue = -99999
+    alpha = -100000
+    beta = 100000
+    for move in board.legal_moves:
+        board.push(move)
+        boardValue = -alphabetaMinimax(board, -beta, -alpha, depth-1)
+        if boardValue > bestValue:
+            bestValue = boardValue
+            bestMove = move
+        if(boardValue > alpha):
+            alpha = boardValue
+        board.pop()
+    return bestMove
+
+def getInput(board):
+    """
+    Ask the user for input and returns the result
+
+    Argument:
+    ---------
+    board : A Board object in which the move will be played
+
+    Return:
+    -------
+    The user input as a string if it is a valid SAN notation and false otherwise
+    """
+    try:
+        move = str(input())
+        return board.parse_san(move)
+    except:
+        return False
+
+
+board = chess.Board()
+print(board)
+
+while not board.is_game_over():
+    print("\nEnter a move: ")
+
+    userMove = getInput(board)
+    while not userMove:
+        print("\nEnter a VALID move: ")
+        userMove = getInput(board)
+
+    board.push(userMove)
+    aiMove = searchNextMove(board, 3)
+    board.push(aiMove)
+    print(board)
