e13fb4065260eec090edc5df97a238c892aa6d9c
Hubar Julien <Julien1941@live.be>
2020-04-18 18:26:50 +0200

    update run.sh

--- ChessGame/run.sh
+++ ChessGame/run.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 #
-#SBATCH --job-name=test
+#SBATCH --job-name=Chess
 #SBATCH --output=res.txt
-#
 #SBATCH --ntasks=1
-#SBATCH --time=10:00
-#SBATCH --mem-per-cpu=100
+#SBATCH --cpus-per-task=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=0-05:00:00
+#SBATCH --mem-per-cpu=10g
+#SBATCH --partition=all
+#SBATCH --gres=gpu:1
 
 python3 deepEvaluator.py
