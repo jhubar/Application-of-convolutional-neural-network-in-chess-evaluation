420caa6684754e4bf6e7373c1391aefa4158f8cc
Hubar Julien <Julien1941@live.be>
2020-04-18 20:11:56 +0200

    test result update

--- ChessGame/run.sh
+++ ChessGame/run.sh
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=0-05:00:00
-#SBATCH --mem-per-cpu=10g
+#SBATCH --mem-per-cpu=32g
 #SBATCH --partition=all
 #SBATCH --gres=gpu:1
 
