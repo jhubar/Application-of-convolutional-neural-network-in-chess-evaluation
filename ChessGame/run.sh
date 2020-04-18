f471967af79b01bd95d650317f41c8c19cc11874
Hubar Julien <Julien1941@live.be>
2020-04-18 17:54:03 +0200

    add run.sh

---
+++ ChessGame/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=SIMP_INT
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=0-05:00:00
+#SBATCH --mem-per-cpu=10g
+#SBATCH --partition=all
+#SBATCH --gres=gpu:1
+
+export SUMO_HOME="/home/phockers/Deep-Learning-Project/ChessGame"
+
+python3 deepEvaluator.py
