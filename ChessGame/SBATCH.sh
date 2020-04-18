3687050dc518ce0120a13a8f743a0b9c9b06ab9e
Hubar Julien <Julien1941@live.be>
2020-04-18 14:59:18 +0200

    update

--- ChessGame/SBATCH.sh
+++ ChessGame/SBATCH.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#
-#SBATCH --job-name=test
-#SBATCH --output=res.txt
-#
+#SBATCH --job-name=JUJU_PIERRE
 #SBATCH --ntasks=1
-#SBATCH --time=10:00
-#SBATCH --mem-per-cpu=100
+#SBATCH --cpus-per-task=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=1:00:00
+#SBATCH --mem-per-cpu=1000
+#SBATCH --partition=PostP
+#SBATCH --gres=gpu:1
 
-srun hostname
-srun sleep 60
+srun python3 deepEvaluator.py
