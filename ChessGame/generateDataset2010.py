c996fefafb6bfa029c61a3b13e5053b683ab9f7b
Hubar Julien <Julien1941@live.be>
2020-04-28 15:59:21 +0200

    update

--- ChessGame/generateDataset2010.py
+++ ChessGame/generateDataset2010.py
@@ -5,19 +5,16 @@ import chess
 import chess.pgn
 
 import argparse
-from tqdm import tqdm
 
 from stockfishEvaluator import StockfishEvaluator
 from deepEvaluator import DeepEvaluator
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-# device = 'cpu'
-print(device)
+
 
 def loadData(isWindows: bool):
     """
     Loads the data from a pgn file
     """
-    filePath = "Data/ficsgamesdb_201001_chess_nomovetimes_127697.pgn"
+    filePath = "ficsgamesdb_201901_CvC_nomovetimes_120511.pgn"
 
     with open(filePath) as pgn:
         nbGames = len(pgn.readlines()) // 22
@@ -28,48 +25,45 @@ def loadData(isWindows: bool):
 
         games = []
         nbStates = 0
+
         game = chess.pgn.read_game(pgn)
 
-        for i in tqdm(range(nbGames), desc="Parsing games", unit="game"):
+        for i in range(nbGames):
             games.append(game)
 
             nbStates += int(game.headers['PlyCount'])
 
             game = chess.pgn.read_game(pgn)
 
-    print("Completed. {} games have been parsed\n".format(len(games)))
-
     X = []
     y = []
 
     stockfish = StockfishEvaluator(isWindows)
 
-    for game in tqdm(games, desc="Generating states", unit="game"):
-        node = game.end().
+    for game in games:
+        node = game.end()
 
         while node.parent is not None:
             position = node.board()
             tensor = DeepEvaluator.boardToTensor(position)
             output = stockfish.evaluate(position)
 
-            X.append(tensor).to(device)
-            y.append(output).to(device)
+            X.append(tensor)
+            y.append(output)
 
             node = node.parent
 
     stockfish.quit()
 
-    print("Completed. {} states have been generated\n".format(len(X)))
-
     return X, y
 
 def save(obj, filePath):
     with open(filePath, "wb") as file:
-        pickle.dump(obj, file).to(device)
+        pickle.dump(obj, file)
 
 def load(filePath):
     with open(filePath, "rb") as file:
-        return pickle.load(file).to(device)
+        return pickle.load(file)
 
 
 
@@ -89,11 +83,9 @@ if __name__ == "__main__":
     # Extract depth
     isWindows = args.windows
 
-    print("############################################################")
-    print("#################### GENERATING DATASET ####################")
-    print("############################################################\n")
+    X, y = loadData(isWindows)
 
-    X, y = loadData(isWindows).to(device)
+    save(X, "chessInput")
+    save(y, "chessOutput")
 
-    save(X, "chessInput2010")
-    save(y, "chessOutput2010")
+    print("Completed. {} states have been generated\n".format(len(X)))
