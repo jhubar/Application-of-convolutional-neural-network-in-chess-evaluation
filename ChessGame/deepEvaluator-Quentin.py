58d88fe5d402ad656db919672782a5b2040e23c7
Quentin Lowette <quentin.lowette@gmail.com>
2020-05-05 13:45:38 +0200

    1024 deq

--- ChessGame/deepEvaluator-Quentin.py
+++ ChessGame/deepEvaluator-Quentin.py
@@ -162,7 +162,7 @@ class DeepEvaluator(Evaluator):
         train_y -= torch.min(train_y)
         train_y /= torch.max(train_y)
 
-        train_data = TensorDataset(train_X[:512], train_y[:512])
+        train_data = TensorDataset(train_X[:1024], train_y[:1024])
 
         return train_data
 
