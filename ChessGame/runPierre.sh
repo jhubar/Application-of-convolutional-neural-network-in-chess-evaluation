20b5fb7497c5fe50b525b58398dec74b6fbe6b99
Lastshadow <pierrohockers@hotmail.com>
2020-05-12 00:09:53 +0200

    dropout 0.2

--- ChessGame/runPierre.sh
+++ ChessGame/runPierre.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 #SBATCH --job-name=Conv
-#SBATCH --output=output-Pierre-Drop01.txt
+#SBATCH --output=output-Pierre-Drop02.txt
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --ntasks-per-node=1
--- ChessGame/runPierreMLP.sh
+++ ChessGame/runPierreMLP.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 #SBATCH --job-name=MLP
-#SBATCH --output=output-Pierre-MLP-Drop01.txt
+#SBATCH --output=output-Pierre-MLP-Drop02.txt
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --ntasks-per-node=1
