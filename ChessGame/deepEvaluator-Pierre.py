40effa43421b9bdcb8c094453ae2caa50ed78bb7
Lastshadow <pierrohockers@hotmail.com>
2020-05-13 15:10:55 +0200

    error corrected

--- ChessGame/deepEvaluator-Pierre.py
+++ ChessGame/deepEvaluator-Pierre.py
@@ -234,6 +234,6 @@ if __name__ == "__main__":
         if epoch % 1 == 0:
             print('Epoch : ', epoch+1, '\t', 'loss :', train_losses[-1])
 
-    plt.plot(train_losses, label='Training loss', 'ro')
+    plt.plot(train_losses, 'ro', label='Training loss')
     plt.legend()
     plt.savefig("Graph/"+stringName)
