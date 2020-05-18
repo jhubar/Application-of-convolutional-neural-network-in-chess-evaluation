82a8c99b3a78515eb35b28d37687156fac839828
Lastshadow <pierrohockers@hotmail.com>
2020-05-18 16:27:16 +0200

    title changed

--- ChessGame/deepEvaluator-Pierre-Pooling.py
+++ ChessGame/deepEvaluator-Pierre-Pooling.py
@@ -28,10 +28,10 @@ print(device)
 
 dropout = 0
 learning_rate = 0.01
-nb_epochs = 10
+nb_epochs = 100
 batch  = 1024
 
-com = "Relu_4layers_Pooling" # additional commentary or smth
+com = "Normalized_Relu_4layers_Pooling" # additional commentary or smth
 stringName  = com + "dropout_" + str(dropout) + "_lr_" + str(learning_rate) + "_epochs_" +  str(nb_epochs) + "_batch_" + str(batch) + ".png"
 
 print(" with dropout = " + str(dropout) + " and learning_rate = " + str(learning_rate) + " for " + str(nb_epochs) + " epochs " + com )
